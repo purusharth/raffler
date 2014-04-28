4501eefe14258246f34b4dc414c82e487671f919
Purusharth Prakash <puru1@umbc.edu>
2014-04-27 20:57:42 -0400

    added db entries, controller, seed entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Entry.create!(name: "Kip")
+Entry.create!(name: "Moe")
+Entry.create!(name: "Larry")
+Entry.create!(name: "Curly")
